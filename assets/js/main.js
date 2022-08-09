fe8e301c71d0b778bc0cf1c0715986bf8eb147b3
Bour1cha <wassim.bauricha@qrcode-protect.com>
2022-08-09 16:09:11 +0200

    porjet section ajouté + mixitup js pour les filtres ajouté

--- assets/js/main.js
+++ assets/js/main.js
@@ -75,3 +75,26 @@ window.addEventListener('DOMContentLoaded' , () => {
 });
 
 // fin de ma fonction pour le SplashScreen
+
+
+// Mixitup filtre
+
+let mixerPortfolio = mixitup('.work__container', {
+  selectors: {
+      target: '.work__card'
+  },
+  animation: {
+      duration: 300
+  }
+});
+
+// lien Active work
+
+const linkWork = document.querySelectorAll('.work__item')
+
+function activeWork() {
+  linkWork.forEach(l=> l.classList.remove('active-work'))
+  this.classList.add('active-work')
+}
+
+linkWork.forEach(l=> l.addEventListener("click" , activeWork))
