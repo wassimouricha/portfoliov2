6a8ec9ac9b5132211279ba3d0532f04b6d78fc7a
Bour1cha <wassim.bauricha@qrcode-protect.com>
2022-08-12 15:59:12 +0200

    temoignages ajouté + swiper js + breakpoint de la section

--- assets/js/main.js
+++ assets/js/main.js
@@ -179,3 +179,24 @@ function navHighlighter() {
 
 
 }
+
+// swiper js , pour le swiper de mes témoignages 
+
+let swiper = new Swiper(".testimonials__container", {
+  spaceBetween: 24,
+  loop: true,
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 48,
+    },
+  },
+});
